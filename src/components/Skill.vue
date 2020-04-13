daa6278b5a690fd89d5a9d449b4a75df55b0c27b
harukasaito <haruka.saito@seattleconsulting.co.jp>
2020-04-13 18:28:34 +0900

    feature/20200413

--- src/components/Skill.vue
+++ src/components/Skill.vue
@@ -1,20 +1,22 @@
 <template>
   <div id="skillSection">
-    <div id="skillTitle">
+    <h1 id="skillTitle">
       Skill Set
-    </div>
+    </h1>
     <div id="skillExplain">
-      ああああああああああああああああああ
-      ああああああああああああああああああ
+      <p>
+        ああああああああああああああああああ
+        ああああああああああああああああああ
+      </p>
+    </div>
+    <div id="gitHubLabel">
+      <p>sample</p>
     </div>
     <a
       id="gitHubLink"
       href="https://github.com/haruka-sa/portfolio"
     >
-      <label
-        id="gitHubLabel"
-        for="name1"
-      >GitHub : https://github.com/haruka-sa/portfolio</label>
+      <p>GitHub : https://github.com/haruka-sa/portfolio</p>
     </a>
   </div>
 </template>
@@ -22,46 +24,48 @@
 <script>
 </script>
 
-<style>
-#skilltSection {
-  background: #fff;
-  width: 100%;
-  height: auto;
-}
+<style lang="scss" scoped>
+  #skilltSection {
+    background: #fff;
+    width: 100%;
+    height: auto;
+    text-align: center;
+  }
 
-#skillTitle {
-  color: #20879f;
-  font-family: 'Noto Sans JP', sans-serif;
-  font-weight: bold;
-  font-size: 18pt;
-  text-shadow: 0 2px 5px rgba(0, 0, 0, 0.5);
-  text-align: center;
-}
+  #skillTitle {
+    color: #20879f;
+    font-family: 'Noto Sans JP', sans-serif;
+    font-weight: bold;
+    font-size: 18pt;
+    padding: 5px;
+    text-shadow: 0 2px 5px rgba(0, 0, 0, 0.5);
+    text-align: center;
+  }
 
-#skillExplain {
-  color: #707070;
-  font-family: 'Noto Sans JP', sans-serif;
-  font-size: 12pt;
-  word-wrap: break-word;
-  line-height: 1.25em;
-  width: 200px;
-  margin: 0 auto;
-}
+  #skillExplain {
+    color: #707070;
+    font-family: 'Noto Sans JP', sans-serif;
+    font-size: 12pt;
+    word-wrap: break-word;
+    line-height: 1.25em;
+    padding: 20px;
+    width: 200px;
+    margin: 0 auto;
+  }
 
-#gitHubLabel {
-  color: #20879f;
-  font-family: 'Noto Sans JP', sans-serif;
-  font-size: 12pt;
-}
+  #gitHubLabel {
+    text-align: center;
+  }
 
-#gitHubLink {
-  color: #20879f;
-  font-family: 'Noto Sans JP', sans-serif;
-}
+  #gitHubLink {
+    color: #20879f;
+    font-family: 'Noto Sans JP', sans-serif;
+    text-align: center;
+  }
 
-#skillList {
-  height: 30px;
-}
+  #skillList {
+    height: 30px;
+  }
 
-@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&display=swap');
+  @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&display=swap');
 </style>
